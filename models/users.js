7890125fbbe3ec21d41e7c3db07dc08313fe52f3
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-24 11:27:46 +0200

    change userSchema in back: add a sub-objet for user's articles

--- models/users.js
+++ models/users.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose')
 
+const ArticleSchema = mongoose.Schema( {
+    author: String,
+    content: String,
+    description: String,
+    title: String,
+    url: String,
+    urlToImage: String,
+    language: String
+})
 const userSchema = mongoose.Schema({
     username: String,
     email: String,
     password: String,
     token: String,
     salt: String, 
+    articles: [ArticleSchema]
 })
 
 const userModel = mongoose.model('users', userSchema)
